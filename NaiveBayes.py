078cca4e6bb5012a4e8b249aad0c648be9eff1dc
cliptic <50213900+cliptic@users.noreply.github.com>
2019-08-16 13:31:24 +0300

    NaiveBayes and AdaBoost spam detector classifier

---
+++ NaiveBayes.py
@@ -0,0 +1,25 @@
+from __future__ import print_function, division
+from builtins import range
+
+from sklearn.naive_bayes import MultinomialNB
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('spambase.data').values
+np.random.shuffle(data)
+
+X = data[:,:48]
+Y = data[:,-1]
+Xtrain = X[:-100,]
+Ytrain = Y[:-100,]
+Xtest = X[-100:,]
+Ytest = Y[-100:,]
+
+model = MultinomialNB()
+model.fit(Xtrain, Ytrain)
+print("Classification rate for NB:", model.score(Xtest, Ytest))
+
+from sklearn.ensemble import AdaBoostClassifier
+model = AdaBoostClassifier()
+model.fit(Xtrain, Ytrain)
+print("Classification rate for AdaBoost:", model.score(Xtest, Ytest))
