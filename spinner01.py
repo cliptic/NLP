783af899aa895d7187f640150b0429ccfe59002a
cliptic <50213900+cliptic@users.noreply.github.com>
2019-09-14 17:43:12 +0300

    Reviews to a list of reviews as lists of words

--- spinner01.py
+++ spinner01.py
@@ -1,18 +1,35 @@
+# Separating html format reviews into text instances and triplets of words.
+
+# build a trigram model
+# (word | previous word, next word)
+
 import nltk
 import numpy as np 
 import pandas as pd 
 from bs4 import BeautifulSoup
 import csv
 
-data = BeautifulSoup(open('electronics/positive.review').read(), features="html.parser")
-data = data.findAll('review_text')
+soup = BeautifulSoup(open('electronics/positive.review').read(), features="html.parser")
 
-data = "<review_text>{}</review_text>".format(data)
 
-soup = BeautifulSoup(data, "html.parser")
-print(soup)
-with open('positive_review.csv', 'wb') as f_output:
-    csv_output = csv.writer(f_output, delimiter='|')
+# data = data.findAll('review_text')
+# partly copied from last code
+text_data = []
+for i in soup.find_all('review_text'):
+	txt = i.get_text() #append messages to list
+	txt = txt.strip()
+	txt = txt.lower()
+	lst_txt = txt.split()
+	text_data.append(lst_txt)
 
-    for review_text in soup.find_all('<review_text>'):
-        csv_output.writerow([td.text for td in review_text.find_all('<review_text>')])
+print(text_data[2])
+'''triple_dictionary = {}
+for i in text_data:
+	count = 0
+	for n in i:
+		if count != 0 and count != (len(i)+1):
+			triple_dictionary[i[n]] = [i[n-1], i[n+1]]
+		count += 1	
+
+print(triple_dictionary)
+'''
