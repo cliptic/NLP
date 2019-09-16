282aa7b7b288513d23825c6dcd396bd0aa604b0d
cliptic <50213900+cliptic@users.noreply.github.com>
2019-09-16 19:57:14 +0300

    Spinner with random word

--- spinner01.py
+++ spinner01.py
@@ -12,66 +12,79 @@ from nltk.tokenize import RegexpTokenizer
 
 tokenizer = RegexpTokenizer(r'\w+')
 
-soup = BeautifulSoup(open('electronics/positive.review').read(), features="html.parser")
-
 
 # data = data.findAll('review_text')
 # partly copied from last code
-text_data = []
-for i in soup.find_all('review_text'):
-	txt = i.get_text() #append messages to list
-	txt = txt.lower()
-	lst_txt = tokenizer.tokenize(txt)
-	# txt = txt.strip()
-	# lst_txt = txt.split()
-	text_data.append(lst_txt)
+html_txt_link = 'electronics/positive.review'
+def html_to_probs_dict(html_txt_link):
+	# open link wth Beautiful soup decoder
+	soup = BeautifulSoup(open(html_txt_link).read(), features="html.parser")
+	text_data = []
 
-# print(text_data[5])
+	# extract review text
+	for i in soup.find_all('review_text'):
+		txt = i.get_text() #append messages to list
+		txt = txt.lower()
+		lst_txt = tokenizer.tokenize(txt)
+		# txt = txt.strip()
+		# lst_txt = txt.split()
+		text_data.append(lst_txt)
 
+	# convert list of reviews to triplets dictionary (word: surrounding doubles)
+	triple_dictionary = {}
+	count_dictionary = {}
+	for i in text_data:
+		count = 0
+		for n in i:
+			if count != 0 and count != (len(i)-1):
+				if n not in triple_dictionary:
+					triple_dictionary[i[count]] = [[i[count-1], i[count+1]]]
+					count_dictionary[i[count]] = 1
+				else:
+					triple_dictionary[i[count]].append([i[count-1], i[count+1]])
+					count_dictionary[i[count]] += 1
+			count += 1	
 
-triple_dictionary = {}
-count_dictionary = {}
-for i in text_data:
-	count = 0
-	for n in i:
-		if count != 0 and count != (len(i)-1):
-			if n not in triple_dictionary:
-				triple_dictionary[i[count]] = [[i[count-1], i[count+1]]]
-				count_dictionary[i[count]] = 1
+	# gives a dictionary with a tuple of two surrounding sorted words and a key, 
+	# which is a nested dictionary that contains the possible middle word
+	# and it's cuont in the corpus data
+	probability_dict = {}
+	tuple_count_dict = {}
+	for key in triple_dictionary.keys():
+		for value in triple_dictionary[key]:
+			value.sort()
+			value = tuple(value)
+			if value not in probability_dict:
+				probability_dict[value] = {key:1}
+				tuple_count_dict[value] = 1
 			else:
-				triple_dictionary[i[count]].append([i[count-1], i[count+1]])
-				count_dictionary[i[count]] += 1
-		count += 1	
+				if key not in probability_dict[value].keys():
+					probability_dict[value][key] = 1
+				else:
+					probability_dict[value][key] += 1
+				tuple_count_dict[value] += 1
+	# Calculate probabilities
+	for key in probability_dict.keys():
+		for value in probability_dict[key]:
+			probability_dict[key][value] = probability_dict[key][value] / tuple_count_dict[key]
+	return probability_dict
 
-print(triple_dictionary["left"])
-print(count_dictionary["left"])
 
-# gives a dictionary with a tuple of two surrounding sorted words and a key, 
-# which is a nested dictionary that contains the possible middle word
-# and it's cuont in the corpus data
-probability_dict = {}
-tuple_count_dict = {}
-for key in triple_dictionary.keys():
-	for value in triple_dictionary[key]:
-		value.sort()
-		value = tuple(value)
-		if value not in probability_dict:
-			probability_dict[value] = {key:1}
-			tuple_count_dict[value] = 1
-		else:
-			if key not in probability_dict[value].keys():
-				probability_dict[value][key] = 1
-			else:
-				probability_dict[value][key] += 1
-			tuple_count_dict[value] += 1
+probs_dict = html_to_probs_dict('electronics/positive.review')
 
-print(probability_dict[('the', 'the')])
-print(tuple_count_dict[('the', 'the')])
+def probability(word, sorted_tuple_of_surr_words):
+	prob_w = probs_dict[sorted_tuple_of_surr_words][word]
+	return prob_w
 
-### Calculate probabilities
-for key in probability_dict.keys():
-	for value in probability_dict[key]:
-		probability_dict[key][value] = probability_dict[key][value] / tuple_count_dict[key]
+print(probability('purchased',('i', 'this')))
 
-print(probability_dict[('the', 'the')])
-print(tuple_count_dict[('the', 'the')])
+# random selection with distribution weights
+
+from random import choices
+
+input_tuple = ('i', 'this')
+population = list(probs_dict[input_tuple])
+weights = probs_dict[input_tuple].values()
+
+print(choices(population, weights))
+print(list(probs_dict[('i', 'this')]))
